ebcd9074daa5e81b58a5ad2370e9706de93b16a2
efrainmejiasc <efrainmejiasc@hotmail.com>
2018-12-20 13:08:03 -0400

    Inicio

---
+++ MvcTutorialPractice/MvcTutorialPractice/Controllers/Login_UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorialPractice.Controllers
+{
+    public class Login_UserController : Controller
+    {
+        // GET: Init
+        public ActionResult InitLogin()
+        {
+            return View();
+        }
+    }
+}
